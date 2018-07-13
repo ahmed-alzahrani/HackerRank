af649b7298def621657185981a96101f05ca7a9d
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-07-12 22:28:21 -0400

    one more STL to go

--- Language Proficiency/C++/STL/Maps-STL.cpp
+++ Language Proficiency/C++/STL/Maps-STL.cpp
@@ -5,19 +5,31 @@
 #include <set>
 #include <map>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-
 int main() {
-    map<string, vector<int>> m;
-    int queryCount;
+    map<string, int> m;
+    int queryCount, grade;
     cin >> queryCount;
 
     for (int i = 0; i < queryCount; i++) {
-      
+      int queryType;
+      string name;
+      cin >> queryType >> name;
+
+      switch (queryType) {
+        case 1:
+          cin >> grade;
+          m[name] += grade;
+          break;
+        case 2:
+          m[name] = 0;
+          break;
+        case 3:
+          cout << m[name] << endl;
+          break;
+      }
     }
-
-
-
     return 0;
 }
